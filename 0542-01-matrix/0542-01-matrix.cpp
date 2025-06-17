c69b7579662bf87d4ed61e982b93ba59429e2e58
akshat-0921 <akshat21lko@gmail.com>
2025-06-17 21:48:24 +0530

    Time: 6 ms (95.31%), Space: 34.5 MB (93.2%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        queue<pair<int,int>> q;
-        vector<vector<int>> ans(m,vector<int>(n,0));
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(mat[i][j]==1) 
-                {
-                    q.push({i,j});
-                    ans[i][j]=INT_MAX;
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> ans(m, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                    q.push({i, j});
                 }
             }
         }
-        auto check=[&](int x,int y){
-            return x>=0&&x<m&&y>=0&&y<n;
-        };
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        while(!q.empty())
-        {
-            auto [x,y]=q.front();
+
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
             q.pop();
-            for(int i=0;i<4;i++)
-            {
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(check(nx,ny)&&mat[nx][ny]==0)
-                {
-                    ans[x][y]=1;
-                }
-                else if(check(nx,ny)&&ans[x][y]+1<ans[nx][ny])
-                {
-                    q.push({nx,ny});
-                    ans[nx][ny]=1+ans[x][y];
+            for (int d = 0; d < 4; ++d) {
+                int nx = x + dx[d], ny = y + dy[d];
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+                    if (ans[nx][ny] > ans[x][y] + 1) {
+                        ans[nx][ny] = ans[x][y] + 1;
+                        q.push({nx, ny});
+                    }
                 }
             }
         }
+
         return ans;
     }
 };
