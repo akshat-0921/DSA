b58317b56003399167c2692e92e5610574f209e1
akshat-0921 <akshat21lko@gmail.com>
2025-06-19 19:42:05 +0530

    Time: 4 ms (57.13%), Space: 18.1 MB (79.03%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> mat(n);
+        vector<int> indegree(n,0);vector<int> topo;
+        for(int i=0;i<pre.size();i++)
+        {
+            indegree[pre[i][1]]++;
+            mat[pre[i][0]].push_back(pre[i][1]);
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++) if(indegree[i]==0) q.push(i);
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it:mat[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        reverse(topo.begin(),topo.end());
+        return topo.size()==n?topo:vector<int>();
+    }
+};
