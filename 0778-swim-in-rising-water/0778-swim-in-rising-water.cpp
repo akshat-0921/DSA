d331902786badf2c0f1a0cc83430581533d5feaa
akshat-0921 <akshat21lko@gmail.com>
2025-06-21 19:41:58 +0530

    Time: 3 ms (88.12%), Space: 12.7 MB (82.56%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({grid[0][0],{0,0}});
-        dis[0][0]=grid[0][0];
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        auto check=[&](int x,int y){
-            return x>=0&&y>=0&&x<n&&y<n;
-        };
-        while(!pq.empty())
-        {
-            auto [wt,it]=pq.top();
-            int x=it.first;
-            int y=it.second;
+        int n = grid.size();
+        vector<vector<int>> dis(n, vector<int>(n, INT_MAX));
+        priority_queue<pair<int, pair<int, int>>, 
+                       vector<pair<int, pair<int, int>>>, 
+                       greater<>> pq;
+
+        pq.push({grid[0][0], {0, 0}});
+        dis[0][0] = grid[0][0];
+
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, 1, 0, -1};
+
+        while (!pq.empty()) {
+            auto [t, coord] = pq.top();
             pq.pop();
-            for(int i=0;i<4;i++)
-            {
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(check(nx,ny)&&grid[nx][ny]<dis[nx][ny])
-                {
-                    if(grid[nx][ny]>wt)
-                    {
-                        dis[nx][ny]=grid[nx][ny];
-                        pq.push({dis[nx][ny],{nx,ny}});
-                    }
-                    else if(dis[nx][ny]>wt)
-                    {
-                        dis[nx][ny]=wt;
-                        pq.push({dis[nx][ny],{nx,ny}});
+            int x = coord.first, y = coord.second;
+
+            if (x == n - 1 && y == n - 1)
+                return t;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                    int newTime = max(t, grid[nx][ny]);
+                    if (newTime < dis[nx][ny]) {
+                        dis[nx][ny] = newTime;
+                        pq.push({newTime, {nx, ny}});
                     }
                 }
             }
         }
-        return dis[n-1][n-1];
+
+        return -1;
     }
 };
