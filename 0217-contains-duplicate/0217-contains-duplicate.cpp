54a4801f981abef8007f224fc67bde5baa853e43
akshat-0921 <akshat21lko@gmail.com>
2025-08-23 09:28:32 +0530

    Time: 111 ms (7.14%), Space: 92.4 MB (26.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> mpp;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]>1) return true;
+        }
+        return false;
+    }
+};
